d8726e39dd70fb8705177dd37f3ab7bfd6750689
papryka1 <kasiukas2@gmail.com>
2021-03-29 16:55:17 +0300

    Dashboard fixed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../config/auth')
+const isEmpty = require('lodash.isempty');
 
 // Models
 const Datatable = require('../models/Datatable')
@@ -15,11 +16,19 @@ router.get('/', auth.checkNotAuthenticated, (req, res) => {
 router.get('/dashboard', auth.ensureAuthenticated, async (req, res) => {
     try {
         const userTables = await Datatable.find({"user": req.user._id}).sort({"dateCreated": -1}).exec()
-        res.render('dashboard', {userTables: userTables})
+        if (isEmptyObject(userTables)) {
+            res.render('dashboard', {userTables: userTables, empty: true})
+        } else {
+            res.render('dashboard', {userTables: userTables, empty: false})
+        }
     } catch (err) {
         console.log(err)
         res.render('dashboard')
     }
 })
+
+function isEmptyObject(obj) {
+    return !Object.keys(obj).length;
+}
 
 module.exports = router
