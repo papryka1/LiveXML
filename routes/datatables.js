f8d84c7c5a74f8fb5b315e06877c77f8c9f17a32
papryka1 <kasiukas2@gmail.com>
2021-04-04 17:48:15 +0300

    DataTable Sharing Done

--- routes/datatables.js
+++ routes/datatables.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../config/auth')
-const isEmpty = require('lodash.isempty');
+const isEmpty = require('lodash.isempty')
+var fs = require('fs');
+
+// XML Builder
+const { create } = require('xmlbuilder2')
+
+// String Builder
+const StringBuilder = require("string-builder");
 
 // Models
 const Datatables = require('../models/Datatable')
@@ -22,7 +29,7 @@ router.get('/presets', auth.ensureAuthenticated, (req, res) => {
     res.render('datatables/presets')
 })
 
-// New DataTable
+// Create User DataTable
 router.post('/', auth.ensureAuthenticated, async (req, res) => {
     if(isEmpty(req.body.fieldName || req.body.fieldValue)) {
         console.log("empty")
@@ -58,18 +65,16 @@ router.post('/', auth.ensureAuthenticated, async (req, res) => {
 
 // Share User DataTable
 router.put('/share/:id', auth.ensureAuthenticated, async (req, res) => {
-    console.log("share " + req.body.shareUserEmail)
-    dataTable = await Datatables.findById(req.params.id).exec()
-    let user
+    console.log("sharing for: " + req.body.shareUserEmail)
+    let dataTable
     try {
-        user = await User.find({ "email": req.body.shareUserEmail}).exec()
+        dataTable = await Datatables.findById(req.params.id).exec()
+        const user = await User.find({ "email": req.body.shareUserEmail}).exec()
         if(user.length > 0) {
-            console.log(user)
-            console.log(user.name)
             try {
                 await Datatables.updateOne(
                     { _id:  dataTable._id },
-                    { $addToSet: { shared: user._id } }
+                    { $addToSet: { shared: user[0]._id } }
                 )
             } catch (err) {
                 console.error(err);
@@ -85,26 +90,46 @@ router.put('/share/:id', auth.ensureAuthenticated, async (req, res) => {
     }
 })
 
+// Unshare other User from Datatable
+router.delete('/unshare/:tableId/user/:userId', auth.ensureAuthenticated, async (req, res) => {
+    try {
+        const dataTable = await Datatables.findById(req.params.tableId).exec()
+        const user = await User.findById(req.params.userId).exec()
+        console.info(dataTable)
+        console.info(user)
+        await Datatables.updateOne(
+            { _id:  dataTable._id },
+            { $pull: { shared: user._id }}
+        )
+        
+        res.redirect(`../../../${dataTable._id}`)  
+    } catch (err) {
+        console.error(err);
+        res.redirect(`/`)  
+    }
+})
+
 // Show User DataTable 
 router.get('/:id', auth.ensureAuthenticated, async (req, res) => {
     try {
         const table = await Datatables.findById(req.params.id).exec()
-        res.render('datatables/datatable', { datatable: table })
+        const sharedUsers = await User.find({_id: table.shared}, {name: 1})
+        res.render('datatables/datatable', { datatable: table, sharedUsers: sharedUsers })
     } catch (err) {
+        console.error(err)
         res.redirect('/dashboard')
     }
 })
 
 // Update DataTable
 router.put('/:id', auth.ensureAuthenticated, async (req, res) => {
-    console.log(req.body)
-    
-    let dataTable
     try {
-        dataTable = await Datatables.findById(req.params.id).exec()
+        const dataTable = await Datatables.findById(req.params.id).exec()
         dataTable.tableName = req.body.dataTableName
-        await dataTable.save()
-        
+        await dataTable.save()  
+
+        //generateXML(dataTable._id, req.body)    
+
         // doing full redirect so that _method=PUT would not be left in URL
         res.redirect(`${dataTable._id}`) 
     } catch {
@@ -124,6 +149,17 @@ router.delete('/:id', auth.ensureAuthenticated, async (req, res) => {
     }
 })
 
+async function generateXML(dataTableID, body) {
+    try {
+        dataTable = await Datatables.findById(dataTableID).exec()
+        console.info("Generating XML for _id: " + dataTable._id)
+        console.log(body)
+        const xmlStr = '<root att="val"><foo><bar>foobar</bar></foo></root>';
+        const sb = new StringBuilder();
 
+    } catch (err) {
+        console.error(err)
+    }     
+}
 
 module.exports = router
